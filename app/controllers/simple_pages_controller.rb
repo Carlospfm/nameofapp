1be77cd84eae20cc752ceb902a888aff32d410f3
Carlos <cponsfuster@gmail.com>
2018-10-13 08:12:00 +0200

    4.5 addinc carousel and product form

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,8 +1,10 @@
 class SimplePagesController < ApplicationController
+	
 	def landing_page
-    @featured_product = Product.first
-  end
-  
-def index
-  end
+	  @products = Product.limit(3)
+	end
+	  
+	def index
+	end
+
 end
