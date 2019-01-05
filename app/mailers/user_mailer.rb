05c2a1c56a5a8b5b1b71e636c13ffecb9742dd2e
Carlos <cponsfuster@gmail.com>
2019-01-05 19:22:44 +0100

    5.11 adding email to new users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,11 +8,17 @@ class UserMailer < ApplicationMailer
          subject: "A new contact form message from #{name}")
   end
 
-      def thank_you
+  def thank_you
         @name = params[:name]
         @email = params[:email]
         @message = params[:message]
         UserMailer.contact_form(@email, @name, @message).deliver_now
-    end
+  end
+
+  def welcome(user)
+    @appname = "BERBI"
+    mail(to: user.email,
+      subject: "Welcome to #{@appname}!")
+  end
     
 end
